793def8d93061b92f03860d1533a180b9734aac7
yuliasemenyuk <semenyukyp@gmail.com>
2024-10-01 13:53:58 +0300

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,48 @@
 class ProjectInput {
-    templateEl: HTMLTemplateElement;
-    hostEl: HTMLDivElement;
-    element: HTMLFormElement
-    constructor() {
-        this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostEl = document.getElementById('app')! as HTMLDivElement;
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEL: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+  constructor() {
+    this.templateEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
-        const importedNode = document.importNode(this.templateEl.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.attach();
-    }
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
 
-    // splitting selection and rendering logic, inserts form into app
-    private attach() {
-        this.hostEl.insertAdjacentElement('afterbegin', this.element)
-    }
+    //Access to the inputs
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputEL = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  // splitting selection and rendering logic, inserts form into app
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this)); //bind fixes addEventListener behaviour related to 'this' context
+  }
+
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputEl.value);
+  }
 }
 
-const ProjInput = new ProjectInput;
-
+const ProjInput = new ProjectInput();
